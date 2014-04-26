4403882bb84b0b4f942f0b2965ee90924e95219d
Leigh <leight@gmail.com>
2014-04-26 13:33:56 +0100

    Add function stubs

--- curve25519.c
+++ curve25519.c
@@ -4,6 +4,64 @@
 
 #include "php.h"
 
+PHP_FUNCTION(curve25519_public)
+{
+	char *secret;
+	int secret_len;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "s", &secret, &secret_len) == FAILURE) {
+		RETURN_FALSE;
+	}
+
+	if (secret_len != 32) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Secret must be 32 bytes");
+		RETURN_FALSE;
+	}
+
+	RETURN_TRUE;
+}
+
+PHP_FUNCTION(curve25519_shared)
+{
+	char *secret;
+	int secret_len;
+
+	char *public;
+	int public_len;
+
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "ss", &secret, &secret_len, &public, &public_len) == FAILURE) {
+		RETURN_FALSE;
+	}
+
+	if (secret_len != 32) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Secret must be 32 bytes");
+		RETURN_FALSE;
+	}
+
+	if (public_len != 32) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Public must be 32 bytes");
+		RETURN_FALSE;
+	}
+
+	RETURN_TRUE;
+}
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_curve25519_public, 0, 0, 1)
+	ZEND_ARG_INFO(0, secret)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_curve25519_shared, 0, 0, 1)
+	ZEND_ARG_INFO(0, secret)
+	ZEND_ARG_INFO(0, public)
+ZEND_END_ARG_INFO()
+
+const zend_function_entry curve25519_functions[] = {
+	PHP_FE(curve25519_public, arginfo_curve25519_public)
+	PHP_FE(curve25519_shared, arginfo_curve25519_shared)
+	PHP_FE_END
+};
+
 PHP_MINFO_FUNCTION(curve25519)
 {
 	php_info_print_table_start();
@@ -14,7 +72,7 @@ PHP_MINFO_FUNCTION(curve25519)
 zend_module_entry curve25519_module_entry = {
 	STANDARD_MODULE_HEADER,
 	"curve25519",
-	NULL,
+	curve25519_functions,
 	NULL,
 	NULL,
 	NULL,
